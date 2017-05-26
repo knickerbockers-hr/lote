1f2544354d691caf3f6745cbc4207205bde30090
Connor K. Wilson <ckwilson42@gmail.com>
2017-05-26 10:58:48 -0700

    Move app start to root

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,3 @@
 const config = require('config');
-// var config = require('config-heroku');
-const env = require('dotenv').config();
 config['knex'].connection = process.env.DATABASE_URL;
 module.exports = config['knex'];
