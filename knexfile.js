9180d1358c121652d6d67e5099fc8648e95632b4
Connor K. Wilson <ckwilson42@gmail.com>
2017-05-25 21:33:17 -0700

    redis connection check for REDIS_URL

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,5 @@
 const config = require('config');
 // var config = require('config-heroku');
 const env = require('dotenv').config();
+config['knex'].connection = process.env.DATABASE_URL;
 module.exports = config['knex'];
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,6 +1,6 @@
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
-const redisClient = require('redis').createClient();
+const redisClient = require('redis').createClient(process.env.REDIS_URL);
 
 module.exports.verify = (req, res, next) => {
   if (req.isAuthenticated()) {
@@ -9,12 +9,16 @@ module.exports.verify = (req, res, next) => {
   res.redirect('/login');
 };
 
+let storeOptions = process.env.REDIS_URL ?
+{client: redisClient} :
+{
+  client: redisClient,
+  host: 'localhost',
+  port: 6379
+};
+
 module.exports.session = session({
-  store: new RedisStore({
-    client: redisClient,
-    host: 'localhost',
-    port: 6379
-  }),
+  store: new RedisStore(storeOptions),
   secret: 'more laughter, more love, more life',
   resave: false,
   saveUninitialized: false
