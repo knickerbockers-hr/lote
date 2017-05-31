94df8014ebc00e6f8ce43eca0fc8b11c5a6a662f
Edward Kim <mr.edward.kim@gmail.com>
2017-05-31 14:45:46 -0700

    Add lotes db model

---
+++ db/models/lotes.js
@@ -0,0 +1,40 @@
+const db = require('../');
+
+const Lote_Sent = db.Model.extend({
+  tableName: 'lotes_sent',
+  lotesReceived: function() {
+    return this.hasMany(Lote_Received);
+  },
+  lote: function() {
+    return this.morphTo('lote', Lote_Text, Lote_Audio);
+  }
+});
+
+module.exports.Lote_Sent = Lote_Sent;
+
+const Lote_Received = db.Model.extend({
+  tableName: 'lotes_received',
+  lotesSent: function() {
+    return this.belongsTo(Lote_Sent);
+  }
+});
+
+module.exports.Lote_Received = Lote_Received;
+
+const Lote_Text = db.Model.extend({
+  tableName: 'lotes_text',
+  lotes_sent: function() {
+    return this.morphMany(Lote_Sent, 'lote');
+  }
+});
+
+module.exports.Lote_Text = Lote_Text;
+
+const Lote_Audio = db.Model.extend({
+  tableName: 'lotes_audio',
+  lotes_sent: function() {
+    return this.morphMany(Lote_Sent, 'lote');
+  }
+});
+
+module.exports.Lote_Audio = Lote_Audio;
