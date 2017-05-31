af8abb9783d84e646d837926dbdae384aa1f0072
Edward Kim <mr.edward.kim@gmail.com>
2017-05-31 15:10:16 -0700

    Add lotes controller and modify routes to use 'profileId' instead of 'id' for profile id

--- server/routes/profiles.js
+++ server/routes/profiles.js
@@ -2,16 +2,22 @@
 const express = require('express');
 const router = express.Router();
 const ProfileController = require('../controllers').Profiles;
+const LoteController = require('../controllers').Lotes;
 
 router.route('/')
   .get(ProfileController.getAll)
   // .post(ProfileController.create)
   ;
 
-router.route('/:id')
+router.route('/:profileId')
   .get(ProfileController.getOne)
   .put(ProfileController.update)
   // .delete(ProfileController.deleteOne)
+  ;
+
+router.route('/:profileId/lotes')
+  .get(LoteController.getAllForProfile)
+  .post(LoteController.create)
   ;
 
 module.exports = router;
