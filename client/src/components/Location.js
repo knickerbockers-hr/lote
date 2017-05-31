5a8cde45b8a17fbd10096d9b0e627d967831a299
Ivana <ivana.he.lili@gmail.com>
2017-05-31 16:36:01 -0700

    add location component

---
+++ client/src/components/Location.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Location extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="location">
+        <h1>Choose a location</h1>
+      </div>
+    );
+  }
+}
+
+export default Location;
