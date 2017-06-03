ad74b1fb8cfcca4ddaaeb765db1784ca33a72a94
Edward Kim <mr.edward.kim@gmail.com>
2017-06-03 15:16:01 -0700

    Display lotes with different styling for sender vs receiver

--- client/src/components/Lotes.js
+++ client/src/components/Lotes.js
@@ -1,12 +1,32 @@
 import React from 'react';
 
 class Lotes extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      senderStyle: {
+        backgroundColor: '#eaeaea',
+        borderRadius: 5,
+        marginTop: 5,
+        padding: 5,
+      },
+      receiverStyle: {
+        backgroundColor: 'pink',
+        borderRadius: 5,
+        marginTop: 5,
+        padding: 5,
+      }
+    };
+  }
+
   render() {
     return (
       <div>
         <h1>Lotes</h1>
         {this.props.lotes.map((lote, i) => {
-          return (<div key={lote.id}>{lote.id} - {lote.lote.message}</div>);
+          return (<div style={lote.sender_id === this.props.profile.id ? this.state.senderStyle : this.state.receiverStyle} key={lote.id}>{lote.lote.message}</div>);
         })}
       </div>
     );
