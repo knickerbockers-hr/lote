32ae7e0c4e5d56165455e73a95cba3a2a6bd7520
Connor K. Wilson <ckwilson42@gmail.com>
2017-06-05 12:05:37 -0700

    add geocoordinates to location table

---
+++ db/migrations/20170605115532_add_geocoordinate.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.table('locations', (table) => {
+    table.float('latitude');
+    table.float('longitude');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.table('locations', (table) => {
+    table.dropColumn('latitude');
+    table.dropColumn('longitude');
+  });
+
+};
