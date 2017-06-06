2f421512f592bf036629256b6e2c13e6ccc9f827
Connor K. Wilson <ckwilson42@gmail.com>
2017-06-06 16:46:59 -0700

    access session userid in socket

--- server/server-sockets.js
+++ server/server-sockets.js
@@ -1,9 +1,15 @@
+const middleware = require('./middleware');
 
 const makeSockets = (server) => {
   const io = require('socket.io')(server);
 
+  io.use((socket, next) => {
+    middleware.auth.session(socket.request, {}, next);
+  });
+
   io.on('connection', function (socket) {
     socket.emit('news', { hello: 'world' });
+    console.log(socket.request.session);
     socket.on('my other event', function (data) {
       console.log(data);
     });
